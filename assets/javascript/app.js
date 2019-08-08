30f9fc1d70252fb1d29c71361e15510ecae307a5
Syed T. Akbar <sakbar01@icloud.com>
2019-08-07 19:19:51 -0500

    development completed!

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,4 @@
+   
     const beatlesTriviaGame = {
 
         domDivStartButton: document.getElementById("start-div-button"),   
@@ -7,6 +8,7 @@
         domDivResult: document.getElementById("result-area"),   
         domSummary: document.getElementById('summary-area'),
         domTimerText: document.getElementById("timer-text"),
+        domVideoArea: document.getElementById("video-area"),
 
         questionIndex: 0,
         triviaQuestion: "",
@@ -15,8 +17,10 @@
         triviaStartTimeOutFunc: "",
         questonIntervalFunc: "",
         questonIntervalVal: 1000,
+        displyResultTimeOut: 5000,
+        startTriviaTimeOut: 500,
 
-        questionTimerVal: 3,
+        questionTimerVal: 0,
 
         answers: [],
         triviaAnswer: "",
@@ -31,37 +35,29 @@
         
         ResetMessage: function  () {
 
-            this.questionTimerVal = 3;
-
-            this.isAnswerCorrect = false;
-            this.isTimerRunning = false;
+            this.isAnswerCorrect = false;            
             this.isTimesUp = true;
 
             this.triviaQuestion = "";
             this.songUrl = ""
             this.triviaAnswer = "";
 
+            this.questionTimerVal = 10,
+
+    
             this.domDivResult.innerHTML = "";
             this.domSummary.innerHTML = "";  
-            this.domDivReStartButton.style.display = "none";            
+            this.domDivReStartButton.style.display = "none";  
+            this.domDivStartButton.style.display = "none";  
+            this.domVideoArea.style.display = "none";
+            
+            this.domDivTriviaQuestion.style.display = "block";
+            this.domTimerText.style.display = "block";
         },
 
         RestartGame: function  () {
 
-            this.questionTimerVal = 3;
-
-            this.isAnswerCorrect = false;
-            this.isTimerRunning = false;
-            this.isTimesUp = true;
-
-            this.triviaQuestion = "";
-            this.songUrl = ""
-            this.triviaAnswer = "";
-
-            this.domDivResult.innerHTML = "";
-            this.domSummary.innerHTML = "";  
-            this.domDivReStartButton.style.display = "none";
-            this.domDivStartButton.style.display = "none";  
+            this.ResetMessage();
 
             this.numOfCorrect = 0;
             this.numOfWrong = 0;
@@ -75,8 +71,8 @@
         [
             { 
               question: "Which of the following songs contributed to the rumor that Paul had died?",
-              possibleAnswers: ["Strawberry Fields Forever", "Hey Jude", "Yesterday", "Penny Lane"], 
-              url: '<iframe width="560" height="315" src="https://www.youtube.com/embed/HtUH9z_Oey8?controls=0;autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>', 
+              possibleAnswers: ["Strawberry Fields Forever", "Hey Jude", "Yesterday", "Penny Lane"],             
+              url: '<iframe width="560" height="315" src="https://www.youtube.com/embed/HtUH9z_Oey8?controls=0;autoplay=1;start=10" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>', 
               answer: "Strawberry Fields Forever"
             },
 
@@ -101,7 +97,7 @@
             { 
                 question: "What Beatles song ends with a chord played on three different pianos and a harmonium?",
                 possibleAnswers: ["Getting Better", "A Day In The Life", "Fixing A Hole", "Let It Be"], 
-                url: '<iframe width="560" height="315" src="https://www.youtube.com/embed/usNsCeOV4GM?controls=0;autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>', 
+                url: '<iframe width="560" height="315" src="https://www.youtube.com/embed/usNsCeOV4GM?controls=0&amp;autoplay=1;start=10" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>', 
                 answer: "A Day In The Life"
             }
         ],
@@ -123,7 +119,6 @@
                 pElem.textContent = possibleAnswers[i];
                                 
                 titleBtnElem.append(pElem);
-
                 this.domDivAnwser.appendChild(titleBtnElem);
                                                
             }
@@ -143,18 +138,15 @@
         
         startTrivia : function() {
             
-            console.log("startTrivia-function " + this.questonIntervalVal);            
-            this.triviaStartTimeOutFunc = setTimeout(this.startTriviaQuestion.bind(beatlesTriviaGame), 1000);   
-            this.domDivStartButton.style.display = "none";                  
+            this.domDivStartButton.style.display = "none";              
+            this.triviaStartTimeOutFunc = setTimeout(this.startTriviaQuestion.bind(beatlesTriviaGame), this.startTriviaTimeOut);   
+                         
             this.genQuestions();
             
         },
           
         startTriviaQuestion : function() {
-            
-            console.log("startTriviaQuestion-function " + this.questonIntervalVal);
-            clearInterval(this.triviaStartTimeOutFunc);
-             
+                                             
             this.domTimerText.innerHTML = "<h6> Time ramaining: " + ("00" + this.questionTimerVal).slice(-2) + " seconds</h6>";
             this.questonIntervalFunc = setInterval(this.timer.bind(beatlesTriviaGame), this.questonIntervalVal);
             
@@ -162,13 +154,13 @@
 
         moveNext : function () {
 
+            clearInterval(this.questonIntervalFunc);
             if (this.questionIndex === this.triviaQuestions.length - 1)
             {
                 this.showSummary();
                 return;
             }
 
-            clearInterval(this.questonIntervalFunc );
             this.questionIndex++;
             this.ResetMessage();
             this.genQuestions();
@@ -197,26 +189,36 @@
 
         showResult:  function() {
 
-            this.domDivAnwser.style.display = "none";
-            this.domDivResult.style.display = "block";
+            this.domDivAnwser.style.display = "none";    
+            this.domTimerText.style.display = "none";        
+            this.domDivTriviaQuestion.style.display = "none";
             
             clearInterval(this.questonIntervalFunc);
 
+            this.domDivResult.style.display = "block";
+
             if (this.isTimesUp === true)
             {
-                this.domDivResult.innerHTML = "<h4>Your time is up!</h4><p>The answer is: " + this.triviaAnswer + "</p>" ;
+                this.domDivResult.innerHTML = "<h4>Your time is up!</h4><p>The answer was: " + this.triviaAnswer + "</p>" ;
             }
             else if (this.isAnswerCorrect === true)
             {
-                this.domDivResult.innerHTML = "<h4>Correct!</h4><p>The answer is: " + this.triviaAnswer + "</p>" ;
+                this.domDivResult.innerHTML = "<h4>Correct!</h4>" ; //<p>The answer is: " + this.triviaAnswer + "</p>"
             }
             else
             {
-                this.domDivResult.innerHTML = "<h4>Nope!</h4><p>The correct answer is: " + this.triviaAnswer + "</p>" ;
+                this.domDivResult.innerHTML = "<h4>Nope!</h4><p>The correct answer was: " + this.triviaAnswer + "</p>" ;
             }
+
+            this.domVideoArea.style.display = "block";
+
+            this.domVideoArea.innerHTML = this.songUrl;
+            this.domVideoArea.focus();
+
+            window.location.hash = '#video-area';
  
             clearTimeout(this.triviaStartTimeOutFunc);
-            this.triviaStartTimeOutFunc = setTimeout(this.moveNext.bind(beatlesTriviaGame), 3000);   
+            this.triviaStartTimeOutFunc = setTimeout(this.moveNext.bind(beatlesTriviaGame), this.displyResultTimeOut);   
             
         },
 
@@ -224,22 +226,23 @@
 
             this.domDivAnwser.style.display = "none";
             this.domDivResult.style.display = "none";
+            this.domVideoArea.style.display = "none";
+            
             this.domDivReStartButton.style.display = "block";
-
-            this.domSummary.innerHTML = "<p><h6><strong>Please see the result below:</strong></h6></p>" +
-                                        "<h4><p>Number of correct answer : " + this.numOfCorrect + "</p></h4>" +
-                                        "<h4><p>Number of wrong answer : " + this.numOfWrong + "</p></h4>" +
-                                        "<h4><p>Number of question unanswered : " + this.numOfUnAnswered + "</p></h4>" ;
-
             this.domSummary.style.display = "block";
-            
-            
+
+            this.domSummary.innerHTML = "<p><h4><strong>Please see the result below:</strong></h4></p>" +
+                                        "<h5><p>Correct answers : " + this.numOfCorrect + "</p></h5>" +
+                                        "<h5><p>Incorrect answers : " + this.numOfWrong + "</p></h5>" +
+                                        "<h5><p>Unanswered : " + this.numOfUnAnswered + "</p></h5>" ;
+                    
         }
     };
 
 
     window.onload = function(){
-        beatlesTriviaGame.ResetMessage();       
+        beatlesTriviaGame.ResetMessage();      
+        beatlesTriviaGame.domDivStartButton.style.display = "block";   
     }
 
     document.getElementById("start-button").addEventListener("click", 
